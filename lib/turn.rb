c3f380a5dc1dffa7e108b121d017f27420dca23f
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-07 18:15:26 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -2,6 +2,12 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
+  if valid_move?(board, index)
+    move (board, index)
+    display_board
+  else
+    turn(board)
+  end
 end
 
 def input_to_index(user_input)
