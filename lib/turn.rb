374b3a4809f60dbcf77a97d648c1c7e4392afec4
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-07 17:41:01 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -7,6 +7,11 @@ def input_to_index(user_input)
     user_input.to_i - 1
 end
 
+def move(board, index, character="X")
+    board[index] = character
+end
+
+
 def display_board(board)
     puts " #{board[0]} | #{board[1]} | #{board[2]} "
     puts "-----------"
