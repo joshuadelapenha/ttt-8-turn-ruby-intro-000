8fc8199641976b5c0f15386c5c2484fb79ebb8cf
plucky-mapping-0074 <joshuadelapenha@yahoo.com>
2020-04-07 16:41:55 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,27 @@
+
+def display_board()
+  
+end
+
+def valid_move?(board, index)
+  if position_taken?(board,index)
+    false
+  elsif index.between?(0, 8)
+    true
+  else
+    false
+  end
+
+end
+
+def position_taken?(board, index)
+    if board[index] == "" || board[index] == " " || board[index] == nil
+      return false
+    else
+      return true
+    end
+end
+
+def move
+  
+end
